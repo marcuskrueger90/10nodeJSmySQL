f06e3c9bc4259a4ebfbe8b35e6f7a4d072b09ce4
Marcus Krueger <marcuskrueger90@gmail.com>
2019-11-02 10:36:03 -0400

    database set up

--- Bamazon.js
+++ Bamazon.js
@@ -1,3 +1,30 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var fs = require ('fs');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+
+    port:3306,
+
+    user: 'root',
+
+    password: 'S0cc3rM4n!69',
+    database: 'bamazon_db'
+});
+
+connection.connect( function(err, res){
+    if (err) throw err;
+
+    showProducts();
+});
+
+function showProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      
+      console.log(res);
+      connection.end();
+    });
+  }
