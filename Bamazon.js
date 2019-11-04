bc6a3dff249614e0f82ff2a43540f03f500bdf08
Marcus Krueger <marcuskrueger90@gmail.com>
2019-11-03 23:39:40 -0500

    responding to inquirer and spitting correct respnse data back out...now to update the db

--- Bamazon.js
+++ Bamazon.js
@@ -15,25 +15,73 @@ var connection = mysql.createConnection({
 
 connection.connect( function(err, res){
     if (err) throw err;
-
-    showProducts();
+console.log(`WELCOME TO BAMAZON!`)
+    purchaseItem();
 });
 
-function showProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      
-      for (var i= 0; i < res.length; i++){
-        console.log(`<-------------->
-        Item ID: ${res[i].item_id}
-        Name: ${res[i].product_name} 
-        Department: ${res[i].department_name}
-        Price: $${res[i].price}
-        ${res[i].stock_quantity} LEFT IN STOCK!`);
-      }
+function purchaseItem(){
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
 
+    inquirer.prompt([
+      {
+        name: 'choice',
+        type: 'rawlist',
+        choices: function(){
+          var itemshere = [];
+          for(var i =0; i< res.length; i++){
+          itemshere.push(`${res[i].product_name}: $${res[i].price} | ${res[i].stock_quantity} left in stock`);
+        }
+        return itemshere;
+      },
+      message: 'Which item would you like to purchase?'
+    },
+    {
+      name:'quantity',
+      type:'input',
+      message:'How many would you like to purchase?'
+
+    },
+    
+  ]).then(function(answer){
+    
+    var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if ((`${res[i].product_name}: $${res[i].price} | ${res[i].stock_quantity} left in stock`) === answer.choice) {
+            chosenItem = res[i];
+            
+          }
+         
+        }
+        
+        if(chosenItem.stock_quantity >= parseInt(answer.quantity)){
+          var amount = parseInt(answer.quantity);
+          console.log(`You purchased ${amount} ${chosenItem.product_name}(s) for a total of $${chosenItem.price*amount}`)
+        }
+  })
+
+  connection.end();
+  
+})};
+
+// function showProducts() {
+//     console.log("Selecting all products...\n");
+//     connection.query("SELECT * FROM products", function(err, res) {
+//       if (err) throw err;
       
-      connection.end();
-    });
-  }
+//       for (var i= 0; i < res.length; i++){
+//         console.log(`<-------------->
+//         Item ID: ${res[i].item_id}
+//         Name: ${res[i].product_name} 
+//         Department: ${res[i].department_name}
+//         Price: $${res[i].price}
+//         ${res[i].stock_quantity} LEFT IN STOCK!`);
+//       }
+
+//       purchaseItem();
+      
+//     });
+//   }
+
+
+  
